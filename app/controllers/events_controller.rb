c14f12575e15aa2853f27d0b75e85a17bfbed82b
Tom ten Thij <git@tomtenthij.nl>
2009-06-20 21:50:05 +0100

    Sign a user up for an event she is creating

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,17 @@
 class EventsController < ApplicationController
   resources_controller_for :events
   before_filter :login_required, :except => [:index, :show]
+  after_filter :sign_up_for_own_event, :only => [:create]
 
   def create
     self.resource = new_resource
     self.resource.owner = current_user
     @resource_saved = resource.save
+  end
+
+  protected
+
+  def sign_up_for_own_event
+    self.resource.users << current_user
   end
 end
