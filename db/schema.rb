d8bb2ed6f4e871f3d52ca4e0e903cdc6d5b4f6db
Graham Clark <graham@ekosphere.net>
2009-06-20 18:11:08 +0100

    javascript fixes

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090620125144) do
+ActiveRecord::Schema.define(:version => 20090620112650) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
